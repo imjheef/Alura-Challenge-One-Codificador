d7932727c8852922c2a4f916a7c0c0f2211bb7bf
Jhef <jdcr26@hotmail.com>
2023-05-26 14:21:15 -0500

    update home

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ let textArea = document.querySelector(".text-area")
 let button = document.querySelector('button')
 let aside = document.querySelector('aside')
 let mensaje = document.querySelector('.message')
+let asideImg = document.querySelector('.imgLateralSpace')
 
-
+function hideImg() {
+    asideImg.style.display = 'none';
+}
 
 function alertMsg(){
   document.querySelector('p').innerHTML = textArea.value
   mensaje.onclick.innerHTML = mensaje
 }
 
+asideImg.onclick = hideImg
 button.onclick = alertMsg;
