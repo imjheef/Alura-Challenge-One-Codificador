6c6fca4ddf774becf57680038bb0b402f0dfb1a8
Jhef <jdcr26@hotmail.com>
2023-05-27 22:21:53 -0500

    button onclick now deletes img

--- index.js
+++ index.js
@@ -2,16 +2,12 @@ let textArea = document.querySelector(".text-area")
 let button = document.querySelector('button')
 let aside = document.querySelector('aside')
 let mensaje = document.querySelector('.message')
-let asideImg = document.querySelector('.imgLateralSpace')
-
-function hideImg() {
-    asideImg.style.display = 'none';
-}
+let lateralSpace = document.querySelector('.imgLateralSpace')
 
 function alertMsg(){
+  lateralSpace.style.display = 'none';
   document.querySelector('p').innerHTML = textArea.value
   mensaje.onclick.innerHTML = mensaje
 }
 
-asideImg.onclick = hideImg
 button.onclick = alertMsg;
