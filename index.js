87b50e41c4da6622e9a4211d88e673775261cc0d
Jhef <jdcr26@hotmail.com>
2023-05-30 01:14:43 -0500

    function encrypt
    started working on function.

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 let textArea = document.querySelector(".text-area")
-let button = document.querySelector('button')
-let aside = document.querySelector('aside')
+let button = document.querySelector('btn')
 let mensaje = document.querySelector('.message')
 let lateralSpace = document.querySelector('.imgLateralSpace')
 
-function alertMsg(){
+/*  La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat" */
+
+
+function encrypt(){
+  let arrayCode = [
+    ["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+    
+}
+
+
+
+/* function alertMsg(){
   lateralSpace.style.display = 'none';
-  document.querySelector('p').innerHTML = textArea.value
+  document.querySelector('textarea').innerHTML = textArea.value
   mensaje.onclick.innerHTML = mensaje
 }
 
-button.onclick = alertMsg;
+button.onclick = alertMsg; */
