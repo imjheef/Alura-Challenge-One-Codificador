5aa043f82adc97ae5593e25ec091afd9b944ee3c
Jhef <jdcr26@hotmail.com>
2023-05-30 01:37:45 -0500

    update js
    Removed old js code
    
    Function almost finished

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 let textArea = document.querySelector(".text-area")
-let button = document.querySelector('btn')
-let mensaje = document.querySelector('.message')
-let lateralSpace = document.querySelector('.imgLateralSpace')
+let message = document.querySelector('.message')
 
 /*  La letra "e" es convertida para "enter"
 La letra "i" es convertida para "imes"
@@ -9,19 +7,22 @@ La letra "a" es convertida para "ai"
 La letra "o" es convertida para "ober"
 La letra "u" es convertida para "ufat" */
 
+function btnEncrypt(){
+  const textEncrypted = encrypt(textArea.value);
+  message.value = textEncrypted;
+  textArea.value = '';
+  message.style.backgroudImage = none;
+}
 
-function encrypt(){
+function encrypt(stringEncrypted){
   let arrayCode = [
     ["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
-    
+    stringEncrypted = stringEncrypted.toLowerCase()
+
+    for (let i = 0; i < arrayCode.length; i++) {
+      if (stringEncrypted.includes(arrayCode[i][0])) {
+        stringEncrypted = stringEncrypted.replaceAll(arrayCode[i][0], arrayCode[i][1])
+      }
+    }
+    return stringEncrypted
 }
-
-
-
-/* function alertMsg(){
-  lateralSpace.style.display = 'none';
-  document.querySelector('textarea').innerHTML = textArea.value
-  mensaje.onclick.innerHTML = mensaje
-}
-
-button.onclick = alertMsg; */
